ca0dbdea40e0ba294d7400af66ba17ed644e90f2
kruger <kevin.gonzalez@ucrso.info>
2025-04-15 17:15:20 -0600

    Estructura inicial del backend

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const v1Router = require("./v1/routes")
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1", v1Router);
+
+app.listen(PORT,() => {
+    console.log(`🚀 Server escuchando en el puerto ${PORT}`)
+})
